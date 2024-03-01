7e86810a3cc1aad09ab7740b2db5a100c035c8f6
ElizabethLeeWH <leewenhui98@gmail.com>
2024-03-01 17:05:25 +0800

    2nd commit

--- template/template/ecommerce/src/main/java/vttp/batch4/csf/ecommerce/controllers/OrderController.java
+++ template/template/ecommerce/src/main/java/vttp/batch4/csf/ecommerce/controllers/OrderController.java
@@ -4,8 +4,6 @@ package vttp.batch4.csf.ecommerce.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
